f0f48f8e44c0db1e227b8ec591c6a242edacfeb0
higumachan <harekumo792154@gmail.com>
2019-05-08 00:55:49 +0900

    freeもやってみる

--- kumalloc-rs/src/main.rs
+++ kumalloc-rs/src/main.rs
@@ -1,23 +1,16 @@
 use std::alloc::{GlobalAlloc, Layout, alloc};
 use std::ptr::null_mut;
-use std::os::raw::c_void;
+use core::ffi::c_void;
 use std::mem;
 use std::fmt;
 
-struct KumAllocator;
 
-unsafe impl GlobalAlloc for KumAllocator {
-    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 { null_mut() }
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
-}
-
-#[global_allocator]
-static A: KumAllocator = KumAllocator;
 
 
 #[link(name = "kumalloc", kind = "static")]
 extern "C" {
     fn kumalloc(size: usize) -> *mut c_void;
+    fn kufree(p: *mut c_void);
 }
 
 #[repr(C)]
@@ -35,15 +28,35 @@ impl fmt::Debug for MemoryManageArea {
 }
 
 
+struct KumAllocator;
+
+unsafe impl GlobalAlloc for KumAllocator {
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
+        kumalloc(_layout.size()) as *mut u8
+    }
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {
+        kufree(_ptr as *mut c_void)
+    }
+}
+
+#[global_allocator]
+static A: KumAllocator = KumAllocator;
+
+
 fn main() {
     unsafe {
         let a: *mut u8 = kumalloc(10) as *mut u8;
         *a = 10;
         println!("{}", mem::size_of::<MemoryManageArea>() as isize);
         println!("{} {:?} {:?}", *a, a, *(a as *mut MemoryManageArea).offset(-1));
+        kufree(a as *mut c_void);
+        println!("{} {:?} {:?}", *a, a, *(a as *mut MemoryManageArea).offset(-1));
     }
 
     unsafe {
-        assert!(alloc(Layout::new::<u32>()).is_null())
+        assert!(!alloc(Layout::new::<u32>()).is_null())
     }
+    let mut v = Vec::new();
+    // This will allocate memory using the system allocator.
+    v.push(1);
 }
